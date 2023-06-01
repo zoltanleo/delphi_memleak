cd75d592e89aaa45a0b9841bc6131d5a53b5d457
Zoltanleo <leobronstain@gmail.com>
2023-06-01 23:42:46 +0300

    001

---
+++ Project1.dpr
@@ -0,0 +1,21 @@
+﻿program Project1;
+
+uses
+  DUnitX.MemoryLeakMonitor.Default,
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form1};
+
+{$R *.res}
+
+begin
+//  {$ifdef debug}
+//  System.ReportMemoryLeaksOnShutdown:= True;
+//  {$endif}
+  Application.Initialize;
+  {$ifdef debug}
+  System.ReportMemoryLeaksOnShutdown:= True;
+  {$endif}
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
